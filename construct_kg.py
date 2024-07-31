d3f75bd04152d1ef88915a3e58d98533890bb6fc
Alfian <alfiannajih@gmail.com>
2024-07-31 13:55:35 +0700

    add loger

--- construct_kg.py
+++ construct_kg.py
@@ -2,7 +2,8 @@ import argparse
 import os
 import json
 
-from src.datasets.neo4j_connection import Neo4JConnection
+from job_recommender import logger
+from job_recommender.dataset.neo4j_connection import Neo4JConnection
 
 def main(args):
     # Create connection to Neo4j
@@ -11,8 +12,8 @@ def main(args):
         user=args.neo4j_user,
         password=args.neo4j_password
     )
-    NODES_PATH = os.path.join(args.input_directory, "nodes").replace("\\", "/")
-    REL_PATH = os.path.join(args.input_directory, "relations").replace("\\", "/")
+    NODES_PATH = os.path.join(args.input_directory, "nodes")
+    REL_PATH = os.path.join(args.input_directory, "relations")
 
     # List all csv files for nodes from input directory
     nodes_csv_files = os.listdir(NODES_PATH)
@@ -20,13 +21,16 @@ def main(args):
     # For loop insert csv files into Neo4j
     for nodes_csv_file in nodes_csv_files:
         if nodes_csv_file.endswith(".csv"):
-            neo4j_conn.load_csv_to_nodes(os.path.join(NODES_PATH, nodes_csv_file).replace("\\", "/"))
+            path = os.path.join(NODES_PATH, nodes_csv_file)
+            neo4j_conn.load_csv_to_nodes(path)
+
+            logger.info("Loaded nodes from {}".format(path))
 
     # List all csv files for relation from input directory
     rels_csv_files = os.listdir(REL_PATH)
 
     # List mapping rel_label to node_labels
-    rel_mapping_path = os.path.join(REL_PATH, "rel_mapping.json").replace("\\", "/")
+    rel_mapping_path = os.path.join(REL_PATH, "rel_mapping.json")
 
     with open(rel_mapping_path, "r") as fp:
         rel_to_nodes = json.load(fp)
@@ -34,7 +38,10 @@ def main(args):
     # For loop insert csv files into Neo4j
     for rels_csv_file in rels_csv_files:
         if rels_csv_file.endswith(".csv"):
-            neo4j_conn.load_csv_to_relations(os.path.join(REL_PATH, rels_csv_file).replace("\\", "/"), rel_to_nodes)
+            path = os.path.join(REL_PATH, rels_csv_file)
+            neo4j_conn.load_csv_to_relations(path, rel_to_nodes)
+
+            logger.info("Loaded relations from {}".format(path))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
