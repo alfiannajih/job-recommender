f4463af83d2992baeaf8a34648d8603103d6a78f
alfiannajih <alfiannajih@gmail.com>
2024-08-08 08:35:00 +0700

    g retriever model

--- src/job_recommender/pipeline/train.py
+++ src/job_recommender/pipeline/train.py
@@ -1,3 +1,177 @@
-class TrainModelPipeline:
-    def __init__():
-        pass
+import os
+import gc
+from tqdm import tqdm
+import torch
+import json
+import pandas as pd
+from torch.utils.data import DataLoader
+from torch.nn.utils import clip_grad_norm_
+import argparse
+import pathlib
+import sys
+
+sys.path.append(str(pathlib.Path(os.path.abspath("."), "src")))
+from job_recommender.model.graph_llm import GraphLLM
+from job_recommender.dataset.resume_dataset import ResumeDataset
+from job_recommender.utils.evaluate import eval_funcs
+from job_recommender.utils.common import _save_checkpoint, _reload_best_model, seed_everything, adjust_learning_rate
+from job_recommender.utils.dataset import collate_fn
+
+def main(args):
+
+    # Step 1: Set up wandb
+    seed = args.seed
+
+    seed_everything(seed=args.seed)
+    print(args)
+
+    dataset = ResumeDataset("dataset/samples")
+
+    # Step 2: Build Node Classification Dataset
+    train_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
+    val_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn)
+    test_loader = DataLoader(dataset, batch_size=args.eval_batch_size, shuffle=False, collate_fn=collate_fn)
+
+    # Step 3: Build Model
+    #args.llm_model_path = llama_model_path[args.llm_model_name]
+    model = GraphLLM(args)#load_model[args.model_name](graph_type=dataset.graph_type, args=args, init_prompt=dataset.prompt)
+    breakpoint()
+    # Step 4 Set Optimizer
+    params = [p for _, p in model.named_parameters() if p.requires_grad]
+    optimizer = torch.optim.AdamW(
+        [{'params': params, 'lr': args.lr, 'weight_decay': args.wd}, ],
+        betas=(0.9, 0.95)
+    )
+    trainable_params, all_param = model.print_trainable_params()
+    print(f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}")
+
+    # Step 5. Training
+    num_training_steps = args.num_epochs * len(train_loader)
+    progress_bar = tqdm(range(num_training_steps))
+    best_val_loss = float('inf')
+
+    for epoch in range(args.num_epochs):
+
+        model.train()
+        epoch_loss, accum_loss = 0., 0.
+
+        for step, batch in enumerate(train_loader):
+
+            optimizer.zero_grad()
+            loss = model(batch)
+            loss.backward()
+
+            clip_grad_norm_(optimizer.param_groups[0]['params'], 0.1)
+
+            if (step + 1) % args.grad_steps == 0:
+                adjust_learning_rate(optimizer.param_groups[0], args.lr, step / len(train_loader) + epoch, args)
+
+            optimizer.step()
+            epoch_loss, accum_loss = epoch_loss + loss.item(), accum_loss + loss.item()
+
+            if (step + 1) % args.grad_steps == 0:
+                lr = optimizer.param_groups[0]["lr"]
+                print({'Lr': lr})
+                print({'Accum Loss': accum_loss / args.grad_steps})
+                accum_loss = 0.
+
+            progress_bar.update(1)
+
+        print(f"Epoch: {epoch}|{args.num_epochs}: Train Loss (Epoch Mean): {epoch_loss / len(train_loader)}")
+        print({'Train Loss (Epoch Mean)': epoch_loss / len(train_loader)})
+
+        val_loss = 0.
+        eval_output = []
+        model.eval()
+        with torch.no_grad():
+            for step, batch in enumerate(val_loader):
+                loss = model(batch)
+                val_loss += loss.item()
+            val_loss = val_loss/len(val_loader)
+            print(f"Epoch: {epoch}|{args.num_epochs}: Val Loss: {val_loss}")
+            print({'Val Loss': val_loss})
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            _save_checkpoint(model, optimizer, epoch, args, is_best=True)
+            best_epoch = epoch
+
+        print(f'Epoch {epoch} Val Loss {val_loss} Best Val Loss {best_val_loss} Best Epoch {best_epoch}')
+
+        #if epoch - best_epoch >= args.patience:
+        #    print(f'Early stop at epoch {epoch}')
+        #    break
+
+    torch.cuda.empty_cache()
+    torch.cuda.reset_max_memory_allocated()
+
+    # Step 5. Evaluating
+    os.makedirs(f'{args.output_dir}/{args.dataset}', exist_ok=True)
+    path = f'{args.output_dir}/{args.dataset}/model_name_{args.model_name}_llm_model_name_{args.llm_model_name}_llm_frozen_{args.llm_frozen}_max_txt_len_{args.max_txt_len}_max_new_tokens_{args.max_new_tokens}_gnn_model_name_{args.gnn_model_name}_patience_{args.patience}_num_epochs_{args.num_epochs}_seed{seed}.csv'
+    print(f'path: {path}')
+
+    model = _reload_best_model(model, args)
+    model.eval()
+    progress_bar_test = tqdm(range(len(test_loader)))
+    with open(path, "w") as f:
+        for step, batch in enumerate(test_loader):
+            with torch.no_grad():
+                output = model.inference(batch)
+                df = pd.DataFrame(output)
+                for _, row in df.iterrows():
+                    f.write(json.dumps(dict(row)) + "\n")
+            progress_bar_test.update(1)
+
+    # Step 6. Post-processing & compute metrics
+    acc = eval_funcs[args.dataset](path)
+    print(f'Test Acc {acc}')
+    print({'Test Acc': acc})
+
+
+def parse_args_llama():
+    parser = argparse.ArgumentParser(description="G-Retriever")
+
+    parser.add_argument("--model_name", type=str, default='graph_llm')
+    parser.add_argument("--project", type=str, default="project_g_retriever")
+    parser.add_argument("--seed", type=int, default=0)
+
+    parser.add_argument("--dataset", type=str, default='expla_graphs')
+    parser.add_argument("--lr", type=float, default=1e-5)
+    parser.add_argument("--wd", type=float, default=0.05)
+    parser.add_argument("--patience", type=float, default=2)
+
+    # Model Training
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--grad_steps", type=int, default=2)
+
+    # Learning Rate Scheduler
+    parser.add_argument("--num_epochs", type=int, default=1000)
+    parser.add_argument("--warmup_epochs", type=float, default=1)
+
+    # Inference
+    parser.add_argument("--eval_batch_size", type=int, default=16)
+
+    # LLM related
+    parser.add_argument("--llm_model_name", type=str, default='7b')
+    parser.add_argument("--llm_model_path", type=str, default='Maykeye/TinyLLama-v0')
+    parser.add_argument("--llm_frozen", type=str, default='False')
+    parser.add_argument("--llm_num_virtual_tokens", type=int, default=10)
+    parser.add_argument("--output_dir", type=str, default='output')
+    parser.add_argument("--max_txt_len", type=int, default=2048)
+    parser.add_argument("--max_new_tokens", type=int, default=1024)
+
+    # GNN related
+    parser.add_argument("--gnn_model_name", type=str, default='gt')
+    parser.add_argument("--gnn_num_layers", type=int, default=4)
+    parser.add_argument("--gnn_in_dim", type=int, default=768)
+    parser.add_argument("--gnn_hidden_dim", type=int, default=1024)
+    parser.add_argument("--gnn_num_heads", type=int, default=4)
+    parser.add_argument("--gnn_dropout", type=float, default=0.0)
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    args = parse_args_llama()
+    main(args)
--- src/job_recommender/utils/common.py
+++ src/job_recommender/utils/common.py
@@ -5,6 +5,11 @@ from sentence_transformers import (
     SentenceTransformer,
     CrossEncoder
 )
+import torch
+import os
+import random
+import numpy as np
+import math
 
 def read_json(path):
     with open(path, "r") as fp:
@@ -23,3 +28,60 @@ def get_rerank_model(path):
     model = CrossEncoder(model_name=path, trust_remote_code=True)
 
     return model
+
+def _save_checkpoint(model, optimizer, cur_epoch, args, is_best=False):
+    """
+    Save the checkpoint at the current epoch.
+    """
+    os.makedirs(f'{args.output_dir}/{args.dataset}', exist_ok=True)
+
+    param_grad_dic = {
+        k: v.requires_grad for (k, v) in model.named_parameters()
+    }
+    state_dict = model.state_dict()
+    for k in list(state_dict.keys()):
+        if k in param_grad_dic.keys() and not param_grad_dic[k]:
+            # delete parameters that do not require gradient
+            del state_dict[k]
+    save_obj = {
+        "model": state_dict,
+        "optimizer": optimizer.state_dict(),
+        "config": args,
+        "epoch": cur_epoch,
+    }
+    path = f'{args.output_dir}/{args.dataset}/model_name_{args.model_name}_llm_model_name_{args.llm_model_name}_llm_frozen_{args.llm_frozen}_max_txt_len_{args.max_txt_len}_max_new_tokens_{args.max_new_tokens}_gnn_model_name_{args.gnn_model_name}_patience_{args.patience}_num_epochs_{args.num_epochs}_seed{args.seed}_checkpoint_{"best" if is_best else cur_epoch}.pth'
+    print("Saving checkpoint at epoch {} to {}.".format(cur_epoch, path))
+    torch.save(save_obj, path)
+
+
+def _reload_best_model(model, args):
+    """
+    Load the best checkpoint for evaluation.
+    """
+    checkpoint_path = f'{args.output_dir}/{args.dataset}/model_name_{args.model_name}_llm_model_name_{args.llm_model_name}_llm_frozen_{args.llm_frozen}_max_txt_len_{args.max_txt_len}_max_new_tokens_{args.max_new_tokens}_gnn_model_name_{args.gnn_model_name}_patience_{args.patience}_num_epochs_{args.num_epochs}_seed{args.seed}_checkpoint_best.pth'
+
+    print("Loading checkpoint from {}.".format(checkpoint_path))
+
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    model.load_state_dict(checkpoint["model"], strict=False)
+
+    return model
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+def adjust_learning_rate(param_group, LR, epoch, args):
+    """Decay the learning rate with half-cycle cosine after warmup"""
+    min_lr = 5e-6
+    if epoch < args.warmup_epochs:
+        lr = LR * epoch / args.warmup_epochs
+    else:
+        lr = min_lr + (LR - min_lr) * 0.5 * (1.0 + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.num_epochs - args.warmup_epochs)))
+    param_group["lr"] = lr
+    return lr
